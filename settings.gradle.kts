bafe95143736b90b25897024d0b511022795029d
Cereal Dev Null <cerealdevnull@gmail.com>
2024-11-11 20:40:04 +0100

    Renamed to monitor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "Script Monitoring"
+include("monitor")
 include("script-sample")
-include("script-core")
