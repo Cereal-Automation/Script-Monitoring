3a2b5cd0f3ce86d45868d7a77e754d444ba596af
Cereal Dev Null <cerealdevnull@gmail.com>
2024-11-14 21:32:04 +0100

    Disable nike repo test for now because of real requests being send.

--- monitoring/src/test/kotlin/com/cereal/script/monitoring/data/TestNikeApiItemRepository.kt
+++ monitoring/src/test/kotlin/com/cereal/script/monitoring/data/TestNikeApiItemRepository.kt
@@ -1,17 +1,12 @@
 package com.cereal.script.monitoring.data
 
-import com.cereal.script.monitoring.data.item.nike.NikeApiItemRepository
-import com.cereal.script.monitoring.domain.models.Item
-import kotlinx.coroutines.runBlocking
-import kotlin.test.Test
-
 class TestNikeApiItemRepository {
-    @Test
-    fun testSuccess() =
-        runBlocking {
-            val repository = NikeApiItemRepository("https://www.nike.com/w/mens-shoes-nik1zy7ok")
-
-            val collectedItems = mutableListOf<Item>()
-            repository.getItems().collect { collectedItems.add(it) }
-        }
+//    @Test
+//    fun testSuccess() =
+//        runBlocking {
+//            val repository = NikeApiItemRepository("https://www.nike.com/w/mens-shoes-nik1zy7ok")
+//
+//            val collectedItems = mutableListOf<Item>()
+//            repository.getItems().collect { collectedItems.add(it) }
+//        }
 }
