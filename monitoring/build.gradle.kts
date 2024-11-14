721ba86e4f683aa69dd557aaecc6ff60f68d298b
Cereal Dev Null <cerealdevnull@gmail.com>
2024-11-14 21:59:58 +0100

    Use api so that tests succeed.

--- monitoring/build.gradle.kts
+++ monitoring/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     api("com.cereal-automation:cereal-sdk:1.4.0:all")
     api("com.cereal-automation:cereal-licensing:1.4.0")
 
-    implementation("com.prof18.rssparser:rssparser:6.0.8")
+    api("com.prof18.rssparser:rssparser:6.0.8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("it.skrape:skrapeit:1.2.2")
