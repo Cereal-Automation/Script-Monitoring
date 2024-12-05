6086b27fedc6c4e9435622544a563bc83bd8a0d6
Cereal Dev Null <cerealdevnull@gmail.com>
2024-12-05 11:27:48 +0100

    Refactor CommandFactory for repository creation
    Decouple repository creation logic by introducing dedicated methods for creating log and notification repositories. This improves code readability and enhances maintainability by encapsulating repository initialization details.

--- monitoring/src/main/kotlin/com/cereal/script/commands/CommandFactory.kt
+++ monitoring/src/main/kotlin/com/cereal/script/commands/CommandFactory.kt
@@ -3,8 +3,10 @@ package com.cereal.script.commands
 import com.cereal.script.commands.monitor.MonitorCommand
 import com.cereal.script.commands.monitor.data.ScriptNotificationRepository
 import com.cereal.script.commands.monitor.domain.ItemRepository
+import com.cereal.script.commands.monitor.domain.NotificationRepository
 import com.cereal.script.commands.monitor.strategy.MonitorStrategy
 import com.cereal.script.core.data.ScriptLogRepository
+import com.cereal.script.core.domain.repository.LogRepository
 import com.cereal.sdk.component.ComponentProvider
 import kotlin.time.Duration
 
@@ -19,13 +21,18 @@ class CommandFactory(
     ): MonitorCommand =
         MonitorCommand(
             itemRepository = itemRepository,
-            notificationRepository =
-                ScriptNotificationRepository(
-                    provider.preference(),
-                    provider.notification(),
-                ),
-            logRepository = ScriptLogRepository(provider.logger(), statusUpdate),
+            notificationRepository = createNotificationRepository(),
+            logRepository = createLogRepository(statusUpdate),
             delayBetweenScrapes = scrapeInterval ?: Duration.ZERO,
             strategies = strategies,
+        )
+
+    private fun createLogRepository(statusUpdate: suspend (message: String) -> Unit): LogRepository =
+        ScriptLogRepository(provider.logger(), statusUpdate)
+
+    private fun createNotificationRepository(): NotificationRepository =
+        ScriptNotificationRepository(
+            provider.preference(),
+            provider.notification(),
         )
 }
