ad89a766894c22150fd47acd6f69b376eb2f5ba4
Cereal Dev Null <cerealdevnull@gmail.com>
2024-12-17 21:21:05 +0100

    Proxy per request.
    Extracted HTTP client configuration into a new `HttpClientFactory` to improve code modularity and maintainability. This reduces duplication and centralizes the logic for creating configured `HttpClient` instances. Adjusted `SnkrsApiClient` to use `HttpClientFactory` for requests.

--- monitoring/src/main/kotlin/com/cereal/script/clients/snkrs/SnkrsApiClient.kt
+++ monitoring/src/main/kotlin/com/cereal/script/clients/snkrs/SnkrsApiClient.kt
@@ -8,27 +8,9 @@ import com.cereal.script.commands.monitor.domain.models.Item
 import com.cereal.script.commands.monitor.domain.models.ItemProperty
 import com.cereal.script.commands.monitor.domain.models.Variant
 import com.cereal.sdk.models.proxy.RandomProxy
-import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.engine.ProxyBuilder
-import io.ktor.client.engine.http
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BasicAuthCredentials
-import io.ktor.client.plugins.auth.providers.basic
-import io.ktor.client.plugins.compression.ContentEncoding
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.client.request.parameter
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
-import one.ifelse.tools.useragent.RandomUserAgent
 import java.math.BigDecimal
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
@@ -39,60 +21,6 @@ class SnkrsApiClient(
 ) {
     private val url = "https://api.nike.com/product_feed/threads/v3/"
     private val defaultCurrencyCode = Currency.USD
-    private val client: HttpClient =
-        HttpClient {
-            engine {
-                randomProxy?.let {
-                    // TODO
-                    proxy = ProxyBuilder.http("http://TODO:TODO")
-                }
-            }
-            install(ContentNegotiation) {
-                json(
-                    Json {
-                        ignoreUnknownKeys = true
-                    },
-                )
-            }
-            install(Logging) {
-                level = LogLevel.BODY
-            }
-            install(HttpTimeout) {
-                requestTimeoutMillis = timeout.inWholeMilliseconds
-            }
-            install(ContentEncoding) {
-                gzip()
-                deflate()
-            }
-            install(Auth) {
-                basic {
-                    credentials {
-                        BasicAuthCredentials(username = "TODO", password = "TODO")
-                    }
-                }
-            }
-            defaultRequest {
-                header(HttpHeaders.ContentType, ContentType.Application.Json)
-                header(HttpHeaders.Accept, ContentType.Application.Json)
-                header(HttpHeaders.AcceptEncoding, "gzip, deflate, br")
-                header(HttpHeaders.AcceptLanguage, "en-GB,en;q=0.9")
-                header("appid", "com.nike.commerce.snkrs.web")
-                header("DNT", "1")
-                header("nike-api-caller-id", "nike:snkrs:web:1.0")
-                header(HttpHeaders.Origin, "https://www.nike.com")
-                header(HttpHeaders.Referrer, "https://www.nike.com/")
-                header("Sec-Fetch-Dest", "empty")
-                header("Sec-Fetch-Mode", "cors")
-                header("Sec-Fetch-Site", "same-site")
-                header(
-                    HttpHeaders.UserAgent,
-                    RandomUserAgent.random({ it.deviceCategory == "mobile" && it.userAgent.contains("Chrome") }),
-                )
-                header(HttpHeaders.CacheControl, "no-cache, no-store, must-revalidate")
-                header(HttpHeaders.Pragma, "no-cache")
-                header(HttpHeaders.Expires, "0")
-            }
-        }
 
     suspend fun getProducts(
         locale: Locale,
@@ -100,7 +28,8 @@ class SnkrsApiClient(
         count: Int,
     ): List<Item> {
         val response =
-            client
+            HttpClientFactory
+                .create(timeout, randomProxy?.invoke())
                 .get(url) {
                     parameter("anchor", anchor)
                     parameter("count", count)
