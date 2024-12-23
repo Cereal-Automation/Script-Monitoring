bd0ff16f4f99cc42311c5722566639f30aad899f
Cereal Dev Null <cerealdevnull@gmail.com>
2024-12-23 20:57:22 +0100

    Update Ktor dependencies to newer versions
    Upgraded Ktor dependencies from version 3.0.1 to 3.0.2 and 3.0.3 where applicable. This ensures the project benefits from the latest fixes and improvements in the Ktor library. No other changes were made to the build configuration.

--- monitoring/build.gradle.kts
+++ monitoring/build.gradle.kts
@@ -21,13 +21,13 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.13")
 
     // Ktor
-    implementation("io.ktor:ktor-client-core:3.0.1")
-    implementation("io.ktor:ktor-client-cio:3.0.1")
-    implementation("io.ktor:ktor-client-logging:3.0.1")
-    implementation("io.ktor:ktor-client-content-negotiation:3.0.1")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.1")
-    implementation("io.ktor:ktor-client-auth:3.0.1")
-    implementation("io.ktor:ktor-client-encoding:3.0.1")
+    implementation("io.ktor:ktor-client-core:3.0.2")
+    implementation("io.ktor:ktor-client-cio:3.0.3")
+    implementation("io.ktor:ktor-client-logging:3.0.2")
+    implementation("io.ktor:ktor-client-content-negotiation:3.0.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.2")
+    implementation("io.ktor:ktor-client-auth:3.0.2")
+    implementation("io.ktor:ktor-client-encoding:3.0.2")
 
     // Support tooling
     implementation("one.ifelse.tools:user-agents:0.1.2")
