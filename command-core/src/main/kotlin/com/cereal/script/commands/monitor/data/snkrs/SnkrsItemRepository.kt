5b96510cc339d3e9ab22103a30a61b3fbbb2605c
Cereal Dev Null <cerealdevnull@gmail.com>
2025-01-01 12:26:05 +0100

    Fix next page token result.

--- command-core/src/main/kotlin/com/cereal/script/commands/monitor/data/snkrs/SnkrsItemRepository.kt
+++ command-core/src/main/kotlin/com/cereal/script/commands/monitor/data/snkrs/SnkrsItemRepository.kt
@@ -3,6 +3,8 @@ package com.cereal.script.commands.monitor.data.snkrs
 import com.cereal.script.commands.monitor.models.Page
 import com.cereal.script.commands.monitor.repository.ItemRepository
 
+private const val PAGE_COUNT = 50
+
 class SnkrsItemRepository(
     private val snkrsApiClient: SnkrsApiClient,
     private val locale: Locale,
@@ -10,7 +12,15 @@ class SnkrsItemRepository(
     override suspend fun getItems(nextPageToken: String?): Page {
         val anchor = nextPageToken?.toInt() ?: 0
 
-        val result = snkrsApiClient.getProducts(locale, anchor, 50)
-        return Page((anchor + 50).toString(), result)
+        val result = snkrsApiClient.getProducts(locale, anchor, PAGE_COUNT)
+
+        val newAnchor =
+            if (result.size == PAGE_COUNT) {
+                (anchor + PAGE_COUNT).toString()
+            } else {
+                null
+            }
+
+        return Page(newAnchor, result)
     }
 }
