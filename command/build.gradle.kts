5db8eee2a5a2a1c5bb379109a793baeabdc7aaf9
Cereal Dev Null <cerealdevnull@gmail.com>
2025-03-17 21:40:15 +0100

    Removed unused dependencies.

--- command/build.gradle.kts
+++ command/build.gradle.kts
@@ -7,10 +7,6 @@ dependencies {
     api("com.cereal-automation:cereal-sdk:1.5.0:all")
     api("com.cereal-automation:cereal-licensing:1.4.0")
 
-    api("com.prof18.rssparser:rssparser:6.0.8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("org.htmlunit:htmlunit:4.7.0")
     api("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
 
     testImplementation(kotlin("test"))
@@ -18,15 +14,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
     testImplementation("io.mockk:mockk:1.13.13")
     testImplementation("com.cereal-automation:cereal-test-utils:1.4.0")
-
-    // Ktor
-    implementation("io.ktor:ktor-client-core:2.3.13")
-    implementation("io.ktor:ktor-client-cio:2.3.13")
-    implementation("io.ktor:ktor-client-logging:2.3.13")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.13")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.13")
-    implementation("io.ktor:ktor-client-auth:2.3.13")
-    implementation("io.ktor:ktor-client-encoding:2.3.13")
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
