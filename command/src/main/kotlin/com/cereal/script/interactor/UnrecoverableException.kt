e025fb0e9686e4a4f00abab36933eabc8af821d3
Cereal Dev Null <cerealdevnull@gmail.com>
2025-03-18 20:50:37 +0100

    PR comments.

--- command/src/main/kotlin/com/cereal/script/interactor/UnrecoverableException.kt
+++ command/src/main/kotlin/com/cereal/script/interactor/UnrecoverableException.kt
@@ -1,5 +1,10 @@
 package com.cereal.script.interactor
 
+/**
+* Exception that indicates an unrecoverable error condition where retrying would not help.
+* When this exception is thrown, retry mechanisms will be bypassed to avoid wasting resources.
+*/
 class UnrecoverableException(
     message: String? = null,
-) : Exception(message)
+    cause: Throwable? = null,
+) : Exception(message, cause)
