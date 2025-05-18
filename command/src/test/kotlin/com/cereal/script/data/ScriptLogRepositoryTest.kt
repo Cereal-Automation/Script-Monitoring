1320d809fa6d4274f1a90256e5ca266363d54581
Cereal Dev Null <cerealdevnull@gmail.com>
2025-05-18 19:10:14 +0200

    Ktlint format.

--- command/src/test/kotlin/com/cereal/script/data/ScriptLogRepositoryTest.kt
+++ command/src/test/kotlin/com/cereal/script/data/ScriptLogRepositoryTest.kt
@@ -20,72 +20,77 @@ class ScriptLogRepositoryTest {
     }
 
     @Test
-    fun `info logs message and updates status`() = runTest {
-        // Given
-        val message = "Test info message"
-        val args = mapOf("key1" to "value1", "key2" to 42)
+    fun `info logs message and updates status`() =
+        runTest {
+            // Given
+            val message = "Test info message"
+            val args = mapOf("key1" to "value1", "key2" to 42)
 
-        // When
-        scriptLogRepository.info(message, args)
+            // When
+            scriptLogRepository.info(message, args)
 
-        // Then
-        val expectedMessage = "Test info message [key1=value1, key2=42]"
-        coVerify(exactly = 1) { loggerComponent.info(expectedMessage) }
-        coVerify(exactly = 1) { statusUpdate(expectedMessage) }
-    }
-
-    @Test
-    fun `debug logs message without updating status`() = runTest {
-        // Given
-        val message = "Test debug message"
-        val args = mapOf("key1" to "value1", "key2" to 42)
-
-        // When
-        scriptLogRepository.debug(message, args)
-
-        // Then
-        val expectedMessage = "Test debug message [key1=value1, key2=42]"
-        coVerify(exactly = 1) { loggerComponent.info(expectedMessage) }
-        coVerify(exactly = 0) { statusUpdate(any()) }
-    }
+            // Then
+            val expectedMessage = "Test info message [key1=value1, key2=42]"
+            coVerify(exactly = 1) { loggerComponent.info(expectedMessage) }
+            coVerify(exactly = 1) { statusUpdate(expectedMessage) }
+        }
 
     @Test
-    fun `info logs message without args`() = runTest {
-        // Given
-        val message = "Test info message without args"
+    fun `debug logs message without updating status`() =
+        runTest {
+            // Given
+            val message = "Test debug message"
+            val args = mapOf("key1" to "value1", "key2" to 42)
 
-        // When
-        scriptLogRepository.info(message, null)
+            // When
+            scriptLogRepository.debug(message, args)
 
-        // Then
-        coVerify(exactly = 1) { loggerComponent.info(message) }
-        coVerify(exactly = 1) { statusUpdate(message) }
-    }
-
-    @Test
-    fun `debug logs message without args`() = runTest {
-        // Given
-        val message = "Test debug message without args"
-
-        // When
-        scriptLogRepository.debug(message, null)
-
-        // Then
-        coVerify(exactly = 1) { loggerComponent.info(message) }
-        coVerify(exactly = 0) { statusUpdate(any()) }
-    }
+            // Then
+            val expectedMessage = "Test debug message [key1=value1, key2=42]"
+            coVerify(exactly = 1) { loggerComponent.info(expectedMessage) }
+            coVerify(exactly = 0) { statusUpdate(any()) }
+        }
 
     @Test
-    fun `info logs message with empty args map`() = runTest {
-        // Given
-        val message = "Test info message with empty args"
-        val args = emptyMap<String, Any>()
+    fun `info logs message without args`() =
+        runTest {
+            // Given
+            val message = "Test info message without args"
 
-        // When
-        scriptLogRepository.info(message, args)
+            // When
+            scriptLogRepository.info(message, null)
 
-        // Then
-        coVerify(exactly = 1) { loggerComponent.info(message) }
-        coVerify(exactly = 1) { statusUpdate(message) }
-    }
+            // Then
+            coVerify(exactly = 1) { loggerComponent.info(message) }
+            coVerify(exactly = 1) { statusUpdate(message) }
+        }
+
+    @Test
+    fun `debug logs message without args`() =
+        runTest {
+            // Given
+            val message = "Test debug message without args"
+
+            // When
+            scriptLogRepository.debug(message, null)
+
+            // Then
+            coVerify(exactly = 1) { loggerComponent.info(message) }
+            coVerify(exactly = 0) { statusUpdate(any()) }
+        }
+
+    @Test
+    fun `info logs message with empty args map`() =
+        runTest {
+            // Given
+            val message = "Test info message with empty args"
+            val args = emptyMap<String, Any>()
+
+            // When
+            scriptLogRepository.info(message, args)
+
+            // Then
+            coVerify(exactly = 1) { loggerComponent.info(message) }
+            coVerify(exactly = 1) { statusUpdate(message) }
+        }
 }
