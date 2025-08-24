32fa21caf56c283c197b7c59cf10333c9d22ddec
Cereal Dev Null <cerealdevnull@gmail.com>
2025-08-24 21:20:11 +0200

    Fix import

--- command-monitoring/src/main/kotlin/com/cereal/command/monitor/data/tgtg/apiclients/TgtgApiClient.kt
+++ command-monitoring/src/main/kotlin/com/cereal/command/monitor/data/tgtg/apiclients/TgtgApiClient.kt
@@ -19,6 +19,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import java.util.UUID
 import kotlin.time.Duration
